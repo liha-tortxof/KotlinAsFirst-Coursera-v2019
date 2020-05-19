113f634188a6ecc6314f6dfb63c029696851997d
liha-tortxof <ahil.foxtrot@gmail.com>
2020-05-20 01:11:39 +0300

    Lesson 1: Increase test precision

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -49,8 +49,10 @@ fun quadraticRootProduct(a: Double, b: Double, c: Double): Double {
  * Пример главной функции
  */
 fun main() {
-    val x1x2 = quadraticRootProduct(1.0, 13.0, 42.0)
-    println("Root product: $x1x2")
+    //val x1x2 = quadraticRootProduct(1.0, 13.0, 42.0)
+    //println("Root product: $x1x2")
+    val result = sqr(7)
+    println("7 * 7 = $result")
 }
 
 /**
@@ -76,7 +78,8 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double =
+    (deg.toDouble() + (min.toDouble() + sec.toDouble() / 60) / 60) * PI / 180
 
 /**
  * Тривиальная
@@ -92,7 +95,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int = number % 1000 / 100
 
 /**
  * Простая
